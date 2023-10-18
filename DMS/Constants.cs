b19378797491ea090177991c747e86fac79a17f0
Dimitar Kyuchukov <dkyuchukov2003@gmail.com>
2023-10-18 08:20:28 +0300

    Add project files.

---
+++ DMS/Constants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS
+{
+    public enum ECliCommands
+    {
+        Help,
+        CreateTable,
+        DropTable,
+        ListTables,
+        TableInfo
+    }
+
+    public enum ESQLCommands
+    {
+        SELECT,
+        WHERE,
+        JOIN,
+        OR,
+        NOT,
+        AND,
+        ORDER_BY,
+        DISTINCT,
+        DELETE,
+        INSERT
+    }
+}
---
+++ DMS/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DMS");
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.Write("Enter a command (or 'exit' to quit): ");
+                string input = Console.ReadLine()!;
+
+                switch ((ECliCommands)Enum.Parse(typeof(ECliCommands), input, true))
+                {
+                    case ECliCommands.Help:
+                        Console.WriteLine($"Available commands: " +
+                                          $"{ECliCommands.CreateTable}, " +
+                                          $"{ECliCommands.DropTable}, " +
+                                          $"{ECliCommands.ListTables}, " +
+                                          $"{ECliCommands.TableInfo}");
+                        break;
+                    case ECliCommands.CreateTable:
+                        Console.WriteLine("Create Table command logic");
+                        break;
+                    case ECliCommands.DropTable:
+                        Console.WriteLine("Drop Table command logic");
+                        break;
+                    case ECliCommands.ListTables:
+                        Console.WriteLine("List Tables command logic");
+                        break;
+                    case ECliCommands.TableInfo:
+                        Console.WriteLine("Table Info command logic");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command. Type 'help' for available commands.");
+                        break;
+                }
+            }
+        }
+    }
+}
