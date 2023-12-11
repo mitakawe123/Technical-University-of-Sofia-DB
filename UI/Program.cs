c98cb67a348da9d15ea38dac2909ec8c95ca9397
Dimitar Kyuchukov <dkyuchukov2003@gmail.com>
2023-12-11 15:36:21 +0200

    Update UI version

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Windows.Forms;
-
 namespace UI
 {
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
         }
     }
 }
